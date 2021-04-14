8a765e63e8dce63841c8818ded6930d7cd10c223
sanchoco <jguuun@gmail.com>
2021-04-14 09:36:41 +0900

    테스트 모듈 삭제

--- src/user/test/test.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get, Headers } from '@nestjs/common';
-
-@Controller('test')
-export class TestController {
-	@Get()
-	authTest(@Headers() header: any) {
-		return { msg: `인증된 기능 사용, ${header.email}` };
-	}
-}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,16 +3,11 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { AuthMiddleware } from 'src/middleware/AuthMiddleware';
-import { TestController } from './test/test.controller';
+
 
 @Module({
 	imports: [TypeOrmModule.forFeature([User])],
 	controllers: [UserController],
 	providers: [UserService]
 })
-export class UserModule implements NestModule {
-	configure(consumer: MiddlewareConsumer) {
-		consumer.apply(AuthMiddleware).forRoutes(TestController);
-	}
-}
+export class UserModule{}
