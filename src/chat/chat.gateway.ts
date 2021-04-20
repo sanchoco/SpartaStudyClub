8fa1d656205573b4fbe6b0abfaa5a8a08b4e9a35
sanchoco <jguuun@gmail.com>
2021-04-20 11:59:55 +0900

    채팅 게이트웨이 코드 정리

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -21,9 +21,13 @@ export class ChatGateway {
 
 	handleConnection(client: any) {
 		this.current();
-		this.chatRepository.find({ order: { date: 'DESC' }, take:5 }).then((log) => {
-			client.send(JSON.stringify({ event: 'before_msg', data: log.reverse() }));
-		});
+		this.chatRepository
+			.find({ order: { date: 'DESC' }, take: 5 })
+			.then((log) => {
+				client.send(
+					JSON.stringify({ event: 'before_msg', data: log.reverse() })
+				);
+			});
 	}
 
 	handleDisconnect(client: any) {
@@ -50,14 +54,14 @@ export class ChatGateway {
 		};
 		this.broadcast(message);
 		try {
-			this.intoDB(message.data);
+			this.saveDB(message.data);
 		} catch {
 			console.log('채팅 기록 DB 저장 에러');
 		}
 		return;
 	}
 
-	intoDB = async (data: Object) => {
+	saveDB = async (data: Object) => {
 		await this.chatRepository.insert(data);
 	};
 
